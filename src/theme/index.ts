dd55111254905ee54be76b41eb6b94e159daa81d
sars2811 <sarthak28200111@gmail.com>
2024-02-18 23:58:40 +0530

    feat: Add Theme.

---
+++ src/theme/index.ts
@@ -0,0 +1,24 @@
+import { ThemeOptions } from '@mui/material/styles';
+
+const themeOptions: ThemeOptions = {
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#ffffff',
+    },
+    secondary: {
+      main: '#5e5adb',
+      dark: '#282828',
+      light: '#e0dff8',
+    },
+    background: {
+      default: '#f4f4ff',
+    },
+    text: {
+      secondary: '#5e5adb',
+      primary: '#8d8d8d',
+    },
+  },
+};
+
+export default themeOptions;
